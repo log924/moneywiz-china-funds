b334d0eaaad46bd709189edfbfe6d1020f4b6451
Log924 <ylge.17@saif.sjtu.edu.cn>
2022-05-07 13:48:51 +0800

    Update funds.py
    Update code on date format

--- funds.py
+++ funds.py
@@ -1,21 +1,24 @@
+from asyncio import wait_for
 import requests
 import json
 import re
 import webbrowser
+from datetime import datetime
 
 
-tickers = ["000251","001511"]  # 以此类推输入基金代码
+tickers = ["000251","001511","001557","001592","005911","005919","110020","163409","001717","002009","005919","010869","163402","519738","001204","007658","002803","002986","005706","009076","163406","501049"]  # 基金代码
 for ticker in tickers:
     url = "http://fundgz.1234567.com.cn/js/%s.js"%ticker
     # 浏览器头
     headers = {'content-type': 'application/json','User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
     r = requests.get(url, headers=headers)
-    content = r.text # 获取数据
+    content = r.text
     pattern = r'^jsonpgz\((.*)\)'
     search = re.findall(pattern, content)
     # 遍历结果
     for i in search:
         data = json.loads(i)
-        # 数据 (data,type(data))
-        moneywiz = "moneywiz://updateholding?symbol={}.CN&price={}&date={}".format(data['fundcode'],data['dwjz'],data['jzrq']) # 生成指令
+        datejzrq = datetime.strptime(data["jzrq"],"%Y-%m-%d")
+        wizjzrq = datetime.strftime(datejzrq,"%Y%m%d")
+        moneywiz = str("moneywiz://updateholding?symbol={}.CN&price={}&date=".format(data['fundcode'],data['dwjz']) + wizjzrq)
         webbrowser.open(moneywiz) # 运行
